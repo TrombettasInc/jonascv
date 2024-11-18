26f0815662437d9d89574bbc0c0c8961c94e46f8
antonia trombetta <antonia.dorazio@gmail.com>
2024-11-18 16:02:56 +0100

    first commit with structure of components and pages

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Photo from "../Components/Photo";
+import Profile from "../Components/Profile";
+import Service from "../Components/Service";
+import ContactForm from "../Components/ContactForm";
+import Banner from "../Components/Banner";
+import Extras from "../Components/Extras";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Photo />
+      <Profile />
+      <Service />
+      <ContactForm />
+      <Banner />
+      <Extras />
+    </div>
+  );
+};
+
+export default Home;
